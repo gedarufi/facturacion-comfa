958ea76805df7faf410cc43dc7f13dbe98ec43af
Germán David Ruiz Figueroa <german.ruiz@inger.co>
2015-04-28 18:54:08 -0500

    Se ajusto el template del login y se implementaron las acciones de login y registrar usuario

--- default/app/controllers/auth_controller.php
+++ default/app/controllers/auth_controller.php
@@ -2,6 +2,8 @@
 /**
 * Controlador que administra el login
 */
+Load::models('usuarios');
+
 class AuthController extends AppController
 {
   public function login() {
@@ -17,5 +19,47 @@ class AuthController extends AppController
         Flash::error("Fallo en la autenticación");
       }
     }
+  }
+
+  public function registrarme() {
+    if (Input::hasPost('userName', 'password', 'repassword', 'edad', 'phone-number')) {
+      $usuario     = new Usuarios();
+
+      $userName    = Input::post('userName');
+      $password    = Input::post('password');
+      $repassword  = Input::post('repassword');
+      $edad        = Input::post('edad');
+      $phoneNumber = Input::post('phone-number');
+
+      $validacion = $usuario->ValidarModelo(
+        $userName, 
+        $password, 
+        $repassword, 
+        $edad, 
+        $phoneNumber
+      );
+
+      if (!isset($validacion)) {
+        //Creamos y guardamos el usuario
+        $usuario->edad     = $edad;
+        $usuario->login    = $userName;
+        $usuario->password = $password;
+        $usuario->telefono = $phoneNumber;
+
+        if ($usuario->create()) {
+          $auth = new Auth("model", "class: Usuarios", "login: $userName", "password: $password");
+
+          Router::redirect("principal/index");
+        } else {
+          Flash::error('Fallo la inserción');
+        }
+      } else {
+        Flash::error($validacion);
+      }
+    }
+  }
+
+  public function after_filter() {
+    View::template("login");
   }
 }
--- default/app/models/usuarios.php
+++ default/app/models/usuarios.php
@@ -4,5 +4,20 @@
 */
 class Usuarios extends ActiveRecord
 {
+  public function ValidarModelo(
+    $userName, $password, 
+    $repassword, $edad, $phoneNumber) {
 
+    $validacion = null;
+
+    if ($edad < 18) {
+      $validacion = 'Usuario menor de edad';
+    } else if ($password != $repassword) {
+      $validacion = 'Las contraseñas no coinciden';
+    } else if ($this->find("login = '$userName'") != false) {
+      $validacion = "El nombre de usuario $userName ya existe";
+    }
+
+    return $validacion;
+  }
 }
